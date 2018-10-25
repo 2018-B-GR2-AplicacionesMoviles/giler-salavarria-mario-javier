cb99d9175c652772b38fd47413a503b0a9f5a9d9
MARIO JAVIER GILER SALAVARRIA <mario.giler@epn.edu.ec>
2018-10-25 15:48:38 -0500

    nuevo proyecto

---
+++ 02-android/app/src/main/java/com/example/mjg70/a2018movilesgr2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mjg70.a2018movilesgr2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
